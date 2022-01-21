9d6be4eedf8addbbd1a6c2cc3e9064fb7c2ebd3a
Håkon <haakon8855@users.noreply.github.com>
2022-01-21 12:55:31 +0100

    Changed example config to easier example

--- configuration.py
+++ configuration.py
@@ -2,28 +2,36 @@
 
 
 class Config():
-    """Fetches the config from a file and returns it in json/py-dict format."""
+    """
+    Fetches the config from a file and returns it in json/py-dict format.
+    """
     @staticmethod
     def get_config(config_file: str):
+        """
+        Fetches the config from the given configuration file path and returns
+        a json-object/python-dictionary containing the values.
+        """
         return {
-            "loss": "cross_entropy",
-            "lrate": 0.1,
-            "wreg": 0.001,
-            "wrt": "L2",
-            "input": 20,
-            "layer1": {
-                "size": 100,
-                "act": "relu",
-                "wr": (-0.1, 0.1),  # weight range
-                "lrate": 0.01,
-            },
-            "layer2": {
-                "size": 5,
-                "act": "relu",
-                "wr": "glorot",  # weight range
-                "br": (0, 1),  # bias range
-            },
-            "layer3": {
-                "type": "softmax",
-            },
+            "loss":
+            "cross_entropy",  # (global) loss function
+            "lrate":
+            0.1,  # (global) learning rate
+            "wreg":
+            0.001,  # (global) weight regularization rate
+            "wrt":
+            "L2",  # (global) weight regularization type
+            "input":
+            20,  # size of input layer (number of input nodes)
+            "layers": [
+                {
+                    "size": 100,
+                    "act": "relu",
+                    "wr": (-0.1, 0.1),  # weight range
+                },
+                {
+                    "size": 5,
+                    "act": "relu",
+                    "wr": (-0.5, 0.5),  # weight range
+                },
+            ],
         }
